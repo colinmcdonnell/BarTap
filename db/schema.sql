55e78d2aa89908e4973aafc6c16e9f16944903c9
Colin McDonnell <colindavidmcdonnell@gmail.com>
2017-10-02 18:39:41 -0500

    first db

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+-- Create a database
+CREATE DATABASE bev_db;
+USE bev_db;
+
+-- Create the table
+CREATE TABLE bev
+(
+id int NOT NULL AUTO_INCREMENT,
+item_name varchar(255) NOT NULL,
+type varchar(255) NOT NULL,
+price decimal(3,2) NOT NULL,
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,56 @@
+-- Insert 
+INSERT INTO ingredients (item_name, type, price)
+	VALUES
+
+	--vodka
+
+	('titos', 'vodka', 5.50),
+	('absolute', 'vodka', 6.50),
+	('stolichnaya', 'vodka', 7.50),
+	('kettle one', 'vodka', 7.50),
+	('grey goose', 'vodka', 8.50),
+
+	--rum
+
+	('captain morgan', 'rum', 6.00),
+	('malibu', 'rum', 6.00),
+	('bacardi', 'rum', 6.00),
+
+	--whiskey
+	 
+	('jim beam', 'whiskey', 5.50),
+	('jack daniels', 'whiskey', 6.50),
+	('bulleit', 'whiskey', 6.50),
+	('crown royal', 'whiskey', 7.50),
+	('jameson', 'whiskey', 7.50),
+	('markers mark', 'whiskey', 7.50),
+
+	--gin
+
+	('tanqueray', 'gin', 6.50),
+	('bombay sapphire', 'gin', 7.50),
+	('hendricks', 'gin', 8.50),
+
+	--scotch
+
+	('dewars', 'scotch', 7.50),
+	('glen livet', 'scotch', 9.00),
+	('johnny walker', 'scotch', 9.50),
+
+	--tequila
+
+	('jose cuervo', 'tequila', 6.50),
+	('hurra durra', 'tequila', 7.50),
+	('patron', 'tequila', 8.50),
+	('don julio', 'tequila', 9.00)
+
+	--beer
+
+	('lone star', 'beer', 4.00),
+	('pabst', 'beer', 4.00),
+	('bud lt', 'beer', 4.00),
+	('miller lt', 'beer', 4.00),
+	('coors lt', 'beer', 4.00),
+	('shiner bock', 'beer', 5.50),
+	('dos xx', 'beer', 5.50),
+	('corona', 'beer', 6.00);

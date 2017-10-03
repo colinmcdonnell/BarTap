6bbdbc691050e840796f25b85affacdadbbb0d50
Nithya Sam <nithyasam@gmail.com>
2017-10-03 00:06:21 -0500

    configured the webpack and the server

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+
+var app = express();
+var PORT = process.env.PORT || 3000; 
+
+app.use(logger("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static("public"));
+
+// -------------------------------------------------
+
+
+// // Any non API GET routes will be directed to our React App and handled by React Router
+// app.get("*", function(req, res) {
+//   res.sendFile(__dirname + "/public/index.html");
+// });
+// 
+
+// -------------------------------------------------
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});

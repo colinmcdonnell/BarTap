1c972879c980ebfa3dd5625f33a58a30abe6e1ac
Nithya Sam <nithyasam@gmail.com>
2017-10-04 13:43:59 -0500

    corrected the class name

--- app/components/Main.js
+++ app/components/Main.js
@@ -3,7 +3,7 @@ import User from './User/User';
 import Categories from './Categories/Categories'
 
 
-export default class Button extends Component {
+export default class Main extends Component {
 	render(){
 		return(<div>
 			<h1 align="center">BARTAP</h1>

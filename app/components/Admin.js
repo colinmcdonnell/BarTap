50114b54e7a9b35bd2544ffb37de7514bc7613b0
Colin McDonnell <colindavidmcdonnell@gmail.com>
2017-10-13 15:24:22 -0500

    pie chart tracks units_sold and item_name

--- app/components/Admin.js
+++ app/components/Admin.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {browserHistory} from 'react-router';
 import User from "./User/User";
 import Table from "./Table/Table";
+import axios from 'axios';
 
 export default class Admin extends Component {
 
@@ -52,6 +53,35 @@ export default class Admin extends Component {
 	genChart(){
 		console.log(this.state.chart);
 		if(this.state.chart === false){
+			axios.get('/sales')
+  			.then(function (response) {
+
+    			// console.log(response.data[0].item_name);
+    	
+    			// 	[
+					// ['Vodka', 20.0],
+					// ['Beer', 25.0],
+					// ['Whiskey', 26.8],
+					// ['Tequila', 12.8],
+					// ['Rum', 8.5],
+					// ['Gin', 6.2],
+					// ['Cordials', 0.7]
+					// ]
+
+					console.log("res length" + response.data.length);
+		let salesData = [];
+
+			for (var i = 0; i < response.data.length; i++) {
+				let tempArr = [];
+				let item_name = response.data[i].item_name;
+				let units_sold = response.data[i].units_sold;
+				tempArr.push(item_name);
+				tempArr.push(units_sold);
+				console.log("temparr : "+ tempArr);
+				salesData.push(tempArr);
+				
+			}
+  			console.log("salesData: "+ salesData);
 			Highcharts.chart('container', {
 				chart: {
 					type: 'pie',
@@ -62,7 +92,7 @@ export default class Admin extends Component {
 					}
 				},
 				title: {
-					text: 'Top product type sales'
+					text: 'PRODUCT SALES'
 				},
 				tooltip: {
 					pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
@@ -81,24 +111,14 @@ export default class Admin extends Component {
 				series: [{
 					type: 'pie',
 					name: 'Browser share',
-					data: [
-					['Vodka', 20.0],
-					['Beer', 25.0],
-					['Whiskey', 26.8],
-					{
-						name: 'Tequila',
-						y: 12.8,
-						sliced: true,
-						selected: true
-					},
-					['Rum', 8.5],
-					['Gin', 6.2],
-					['Cordials', 0.7]
-					]
+					data: salesData
 				}]
-			});	  
+			});	
+})
+  			.catch(function (error) {
+    			console.log(error);
+  			});
 			this.setState({chart: true});
-
 		}
 		else {
 			document.getElementById("container").innerHTML = "";
